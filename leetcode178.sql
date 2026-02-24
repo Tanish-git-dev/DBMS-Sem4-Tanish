b39510d390e4b3841533444fa9bb27c01c310319
Tanish-git-dev <176894406+Tanish-git-dev@users.noreply.github.com>
2026-02-24 12:50:53 +0530

    Update leetcode178.sql

--- leetcode178.sql
+++ leetcode178.sql
@@ -1,3 +1,3 @@
-select score, dense_rank() over (order by score desc) as "rank"
-from Scores
-order by score desc;
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS "rank"
+FROM Scores
+ORDER BY score DESC;
