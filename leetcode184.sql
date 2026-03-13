ac904abe5ac66324d999124f230b411f49187817
Tanish-git-dev <176894406+Tanish-git-dev@users.noreply.github.com>
2026-03-13 11:02:56 +0530

    Refactor SQL query for better readability

--- leetcode184.sql
+++ leetcode184.sql
@@ -1,9 +1,17 @@
 WITH Temp AS (
-    SELECT *, RANK() OVER ( PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+    SELECT
+      *,
+      RANK() OVER (
+        PARTITION BY departmentId
+        ORDER BY salary DESC
+      ) AS rnk
     FROM Employee
-)
-SELECT d.name AS Department, t.name AS Employee, Salary
+  )
+SELECT
+  d.name AS Department,
+  t.name AS Employee,
+  Salary
 FROM Department d
-INNER JOIN Temp t ON t.departmentId = d.id
+  INNER JOIN Temp t ON t.departmentId = d.id
 WHERE rnk = 1
 ORDER BY t.id ASC
